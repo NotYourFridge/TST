2eaadd41826074f2dc04d6b96d045c25cf3a963b
NotYourFridge <romanopoeran@gmail.com>
2024-01-19 20:17:23 +0100

    commit san

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
         options.AddPolicy("AllowLocalhost",
     builder =>
     {
-        builder.WithOrigins("http://localhost:3000")
+        builder.WithOrigins("http://localhost:3000", "http://127.0.0.1:5500")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials()
